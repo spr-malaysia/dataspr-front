3c6cb77f8fe2fe60873bcd15eaee92038a42ea69
Len E-Herng <49067812+leneherng@users.noreply.github.com>
2024-12-19 23:31:09 +0800

    feat: dark mode shadow

--- dashboards/elections/filter.tsx
+++ dashboards/elections/filter.tsx
@@ -1,9 +1,14 @@
+import Button from "@components/Button";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { useTranslation } from "@hooks/useTranslation";
-import Button from "@components/Button";
-import { WindowContext } from "@lib/contexts/window"
+import { WindowContext } from "@lib/contexts/window";
 import { clx } from "@lib/helpers";
-import { FunctionComponent, MouseEventHandler, useContext, useMemo } from "react";
+import {
+  FunctionComponent,
+  MouseEventHandler,
+  useContext,
+  useMemo,
+} from "react";
 
 /**
  * Election Explorer - Filter
@@ -14,16 +19,23 @@ interface ElectionFilterProps {
   onClick?: MouseEventHandler<HTMLButtonElement> | (() => void);
 }
 
-const ElectionFilter: FunctionComponent<ElectionFilterProps> = ({ onClick }) => {
+const ElectionFilter: FunctionComponent<ElectionFilterProps> = ({
+  onClick,
+}) => {
   const { t } = useTranslation();
   const { scroll } = useContext(WindowContext);
   const show = useMemo(() => scroll.y > 300, [scroll.y]);
 
   return (
-    <div className={clx(show ? "fixed right-3 top-16 z-20 lg:hidden" : "hidden")}>
-      <Button onClick={onClick} className="btn-default shadow-floating">
+    <div
+      className={clx(show ? "fixed right-3 top-16 z-20 lg:hidden" : "hidden")}
+    >
+      <Button
+        onClick={onClick}
+        className="btn-default shadow-floating drop-shadow-xl dark:drop-shadow-[0_8px_8px_rgba(255,255,255,0.15)]"
+      >
         <span>{t("filters")}</span>
-        <span className="bg-primary dark:bg-primary-dark w-4.5 h-5 rounded-md text-center text-white">
+        <span className="bg-primary dark:bg-primary-dark w-4.5 h-5 rounded-full text-center text-white">
           3
         </span>
         <ChevronDownIcon className="-mx-[5px] h-5 w-5" />
