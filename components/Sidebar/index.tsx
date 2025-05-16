d94e0b0b7e7694435a33a43820aaeb1da3930b2a
root <data.spr.msia@gmail.com>
2025-05-16 14:22:23 +0800

    style: fix button position

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -77,7 +77,7 @@ const Sidebar: FunctionComponent<SidebarProps> = ({ children, categories, onSele
         {/* Mobile */}
         <div className="relative w-full">
           <>
-            <div className="absolute top-20 block lg:hidden">
+            <div className="absolute top-[72px] block lg:hidden">
               <Button
                 className="btn-default sticky top-36 z-10"
                 icon={<Bars3BottomLeftIcon className="h-4 w-4" />}
